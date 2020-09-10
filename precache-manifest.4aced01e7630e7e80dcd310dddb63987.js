a3cb6d48ce2ce6602e15d3cf0e523e82127270fd
MaTo <ily4368@gmail.com>
2020-09-10 11:40:31 +0300

    Updates

---
+++ precache-manifest.4aced01e7630e7e80dcd310dddb63987.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4212ec95b9d39f9dadcd5854c17341f5",
+    "url": "/template-task/index.html"
+  },
+  {
+    "revision": "cd9c838e16690ca0116b",
+    "url": "/template-task/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4030c40993e0df896f2f",
+    "url": "/template-task/static/js/2.69a9c913.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/template-task/static/js/2.69a9c913.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd9c838e16690ca0116b",
+    "url": "/template-task/static/js/main.d6e805ed.chunk.js"
+  },
+  {
+    "revision": "0532ff7fb7304163c79d",
+    "url": "/template-task/static/js/runtime-main.dc8d40ee.js"
+  }
+]);
