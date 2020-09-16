a8049b225739d5a86a3aba83bd7c852c9d347529
MaTo <ily4368@gmail.com>
2020-09-16 10:07:35 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@ if (typeof importScripts === 'function') {
   if (workbox) {
     console.log('Workbox is loaded');
     workbox.core.skipWaiting();
-    
+
     workbox.core.setCacheNameDetails({
       prefix: 'template-task',
       suffix: 'v1',
@@ -15,12 +15,6 @@ if (typeof importScripts === 'function') {
     /* injection point for manifest files. */
     workbox.precaching.precacheAndRoute([{"revision":"2699fe8a142b6174832b8d21c2f35067","url":"index.html"},{"revision":"9584bea79d6514c9426a61ab45744605","url":"precache-manifest.9584bea79d6514c9426a61ab45744605.js"},{"revision":"1f95f3acbbd94dbfb19f47282b70183c","url":"static/css/main.cf595d69.chunk.css"},{"revision":"aff82506238a96728c367da08fad6803","url":"static/js/2.a61baf34.chunk.js"},{"revision":"373ba821abfa26a6743a755ef206197b","url":"static/js/3.9a99ce19.chunk.js"},{"revision":"669f9613136504f4f74a6939a2c0adbc","url":"static/js/4.5a33cad7.chunk.js"},{"revision":"72891cabffc7fdafb3cc704a3f366465","url":"static/js/main.8e57a97b.chunk.js"},{"revision":"23c0e5743a9537ce3de57b01df319130","url":"static/js/runtime-main.53d7e5c6.js"}]);
     /* custom cache rules */
-    workbox.routting.registerRoute(
-      ({url}) => url.origin === 'https://1mato.github.io/template-task/',
-      workbox.strategies.StaleWhileRevalidate({
-        cacheName: 'SWR-template-task'
-      })
-    )
 
   } else {
     // console.log('Workbox could not be loaded. No Offline support');
