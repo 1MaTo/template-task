26106f9a9f74c5130dccd528afdee1e2b9ff105d
MaTo <ily4368@gmail.com>
2020-09-16 10:05:21 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -3,8 +3,8 @@ if (typeof importScripts === 'function') {
   /* global workbox */
   if (workbox) {
     console.log('Workbox is loaded');
-    workbox.setConfig({debug : true})
     workbox.core.skipWaiting();
+    
     workbox.core.setCacheNameDetails({
       prefix: 'template-task',
       suffix: 'v1',
