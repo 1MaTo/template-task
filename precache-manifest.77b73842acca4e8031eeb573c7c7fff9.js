20f510cbc732ee35f0fbc6423571ef17f82b2918
MaTo <ily4368@gmail.com>
2020-09-16 12:09:26 +0300

    Updates

--- precache-manifest.77b73842acca4e8031eeb573c7c7fff9.js
+++ precache-manifest.77b73842acca4e8031eeb573c7c7fff9.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "995d6e1dc671d64efa63f00caa040016",
+    "revision": "0d7f3e55c596a9c59a30a38a681250d9",
     "url": "/template-task/index.html"
   },
   {
-    "revision": "1b9079e2c06a14070071",
+    "revision": "8981afa16911e3998e87",
     "url": "/template-task/static/css/main.cf595d69.chunk.css"
   },
   {
@@ -24,8 +24,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/template-task/static/js/4.5a33cad7.chunk.js"
   },
   {
-    "revision": "1b9079e2c06a14070071",
-    "url": "/template-task/static/js/main.ee7b1e52.chunk.js"
+    "revision": "8981afa16911e3998e87",
+    "url": "/template-task/static/js/main.dcc20933.chunk.js"
   },
   {
     "revision": "b9c0201006516c0a5875",
--- service-worker.js
+++ service-worker.js
@@ -23,7 +23,7 @@ if (typeof importScripts === 'function') {
       googleAnalytics: 'template-google-analytics-name'
     })
 
-    precacheAndRoute([{"revision":"995d6e1dc671d64efa63f00caa040016","url":"index.html"},{"revision":"10628010076f4f5a465df3fc0a400818","url":"precache-manifest.10628010076f4f5a465df3fc0a400818.js"},{"revision":"1f95f3acbbd94dbfb19f47282b70183c","url":"static/css/main.cf595d69.chunk.css"},{"revision":"aff82506238a96728c367da08fad6803","url":"static/js/2.a61baf34.chunk.js"},{"revision":"373ba821abfa26a6743a755ef206197b","url":"static/js/3.9a99ce19.chunk.js"},{"revision":"669f9613136504f4f74a6939a2c0adbc","url":"static/js/4.5a33cad7.chunk.js"},{"revision":"206631a27bf880bb23ea87a9a2fba9fd","url":"static/js/main.ee7b1e52.chunk.js"},{"revision":"23c0e5743a9537ce3de57b01df319130","url":"static/js/runtime-main.53d7e5c6.js"}]);
+    precacheAndRoute([{"revision":"0d7f3e55c596a9c59a30a38a681250d9","url":"index.html"},{"revision":"77b73842acca4e8031eeb573c7c7fff9","url":"precache-manifest.77b73842acca4e8031eeb573c7c7fff9.js"},{"revision":"1f95f3acbbd94dbfb19f47282b70183c","url":"static/css/main.cf595d69.chunk.css"},{"revision":"aff82506238a96728c367da08fad6803","url":"static/js/2.a61baf34.chunk.js"},{"revision":"373ba821abfa26a6743a755ef206197b","url":"static/js/3.9a99ce19.chunk.js"},{"revision":"669f9613136504f4f74a6939a2c0adbc","url":"static/js/4.5a33cad7.chunk.js"},{"revision":"1c6f4385647cf733debb16725fcb7921","url":"static/js/main.dcc20933.chunk.js"},{"revision":"23c0e5743a9537ce3de57b01df319130","url":"static/js/runtime-main.53d7e5c6.js"}]);
 
     registerRoute(
       'https://jsonplaceholder.typicode.com/todos/1',
